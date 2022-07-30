143344174eed507767758f7b526dd19048c1dd8d
NyxCode <moritz.bischof1@gmail.com>
2022-07-31 01:31:47 +0200

    1 binaries uploaded

--- src/lib/Pages.svelte
+++ src/lib/Pages.svelte
@@ -2,7 +2,7 @@
   import Page from "./Page.svelte";
   import {onMount} from "svelte";
 
-  const PRERENDER_MARGIN = "70%";
+  const PRERENDER_MARGIN = "30%";
 
   let pages = [...Array(12).keys()].map(index => ({
     index,
@@ -10,29 +10,20 @@
     element: null
   }));
   let wrapper;
-  let observer;
 
   onMount(() => {
-    pages.forEach(page => page.element.getElement().setAttribute("index", page.index));
+    pages.forEach(page => page.element.setAttribute("index", page.index));
 
-    observer = new IntersectionObserver((a, b) => {
+    let observer = new IntersectionObserver((a, b) => {
       a.forEach(entry => {
         let index = parseInt(entry.target.getAttribute("index"));
-        console.log(index);
         pages[index].show = entry.isIntersecting;
-        pages = pages;
       });
     }, {root: wrapper, rootMargin: `${PRERENDER_MARGIN} 0%`});
     
-    console.log(pages);
-    pages.forEach(page => observer.observe(page.element.getElement()));
-
-    let interval = setInterval(() => {
-      console.log("visible", pages.filter(p => p.show).map(p => p.index))
-    }, 500);
+    pages.forEach(page => observer.observe(page.element));
 
     return () => {
-      clearInterval(interval);
       observer.disconnect();
     };
   });
@@ -40,7 +31,7 @@
 
 <div bind:this={wrapper} class="pages-scrollbar flex flex-col items-center w-full overflow-auto">
   {#each pages as page, i}
-    <Page index={i} bind:this={page.element} show={pages[i].show} />
+    <Page bind:page />
   {/each}
 </div>
 
